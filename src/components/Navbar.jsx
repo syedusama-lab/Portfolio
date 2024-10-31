3d6b0d2d8b7c67385217b77113b2304c5314f835
syedusama <usamasyed237@gmail.com>
2024-10-31 22:58:34 +0500

    change in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center md:pr-5 font-bold">
         <div className="flex gap-x-1 ">
           <div className="mr-0 md:h-12 md:w-12 h-10 w-10">
-            <img src="public/assets/portfolio.png" alt="" className="w-full" />
+            <img src="/assets/portfolio.png" alt="" className="w-full" />
           </div>
           <div
             onClick={() => scrollToSection("#main")}
