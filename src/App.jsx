94e069c9d0b8d6a7a09bfdfb2756de0b4b9954d5
syedusama <usamasyed237@gmail.com>
2024-11-01 10:04:59 +0500

    add lenis for smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,29 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Certifications from "./components/Certification";
 import ContactUs from "./components/ContactUs";
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => t,
+      smooth: true,
+      direction: "vertical",
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div className="App mt-14">
       <Navbar />
@@ -18,7 +39,6 @@ function App() {
       <Projects />
       <Certifications />
       <ContactUs />
-      {/* Other sections go here */}
     </div>
   );
 }
